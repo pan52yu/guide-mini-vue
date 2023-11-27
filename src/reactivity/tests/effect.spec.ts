cae8bc958d87504134c1428fcd04f3498550312a
huyu <yhu8836@gmail.com>
2023-11-27 17:14:31 +0800

    feat: effect.scheduler

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,35 +1,62 @@
-import { effect } from "../effect";
-import { reactive } from "../reactive";
+import { effect } from "../effect"
+import { reactive } from "../reactive"
 
 describe("effect", () => {
   it("happly path", () => {
     const user = reactive({
-      age: 10
+      age: 10,
     })
-    
-    let nextAge;
+
+    let nextAge
     effect(() => {
-      nextAge = user.age + 1  
+      nextAge = user.age + 1
     })
 
     expect(nextAge).toBe(11)
 
     // update
-    user.age++ 
+    user.age++
     expect(nextAge).toBe(12)
   })
 
-  it('should return runner when call effect', () => {
+  it("should return runner when call effect", () => {
     // 1. effect(fn) -> function(runner) -> fn -> return
     let foo = 10
     const runner = effect(() => {
-      foo++;
+      foo++
       return "foo"
     })
 
     expect(foo).toBe(11)
     const r = runner()
     expect(foo).toBe(12)
-    expect(r).toBe('foo')
-  });
+    expect(r).toBe("foo")
+  })
+
+  it("scheduler", () => {
+    // 1. 通过 effect 的第二个参数给定的 一个 scheduler 的 fn
+    // 2. effect 第一次执行的时候 还有执行 fn
+    // 3. 当响应式对象 set update 的时候 不会触发 fn 而是触发 scheduler
+    // 4. 当调用 runner 的时候 会触发 fn
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+    run()
+    expect(dummy).toBe(2)
+  })
 })
