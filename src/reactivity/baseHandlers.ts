83a45f7f038877b44577dbadb9991dd60f1553d6
huyu <yhu8836@gmail.com>
2023-11-28 14:57:19 +0800

    feat: readonly

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,37 @@
+import { track, trigger } from "./effect"
+
+const get = createGetter()
+const set = createSetter()
+const readonlyGet = createGetter(true)
+
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key)
+    if (!isReadonly) {
+      // 收集依赖
+      track(target, key)
+    }
+    return res
+  }
+}
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value)
+    // 触发依赖
+    trigger(target, key)
+    return res
+  }
+}
+
+export const mutableHandler = {
+  get,
+  set
+}
+
+export const readonlyHandler = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`key:${key} set failed`)
+    return true
+  }
+}
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,19 +1,13 @@
-import { track, trigger } from "./effect"
+import { mutableHandler, readonlyHandler } from "./baseHandlers"
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
+  return createReactiveObject(raw, mutableHandler)
+}
 
-      // 收集依赖
-      track(target, key)
-      return res
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-      // 触发依赖
-      trigger(target, key)
-      return res
-    },
-  })
+export function readonly(raw) {
+  return createReactiveObject(raw, readonlyHandler)
+}
+
+function createReactiveObject(raw, baseHandler) {
+  return new Proxy(raw, baseHandler)
 }
